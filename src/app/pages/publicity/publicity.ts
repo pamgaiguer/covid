bfe890c645dd18b6b1a89168df311e0cc89e433e
Pam Gaiguer <pamella.gaiguer@gmail.com>
2020-10-06 11:38:02 +1000

    updating 'na midia' section with the file from google drive

--- src/app/pages/publicity/publicity.ts
+++ src/app/pages/publicity/publicity.ts
@@ -1,5 +1,359 @@
 export const publicity =[ 
     {
+        link: "https://www.lemonde.fr/international/article/2020/08/14/au-bresil-plus-de-105-000-morts-et-un-president-gueri-grace-a-la-chloroquine_6048948_3210.html",
+        titulo: "Au Brésil plus de 105000 morts et un président guéri grâce à la cloroquine",
+        data: "14/8/20",
+        fonte: "Le Monde"
+      },
+      {
+        link: "https://www1.folha.uol.com.br/educacao/2020/08/simulacao-calcula-que-ate-46-de-alunos-podem-ser-infectados-dois-meses-apos-volta-as-aulas.shtml",
+        titulo: "Simulação calcula que até 46% e alunos podem ser infectados dois meses após volta às aulas",
+        data: "21/8/20",
+        fonte: "Folha de S. Paulo"
+      },
+      {
+        link: "https://www.uol.com.br/vivabem/noticias/redacao/2020/08/24/bolhas-de-protecao-local-podem-ter-freado-covid-19-em-sao-paulo.htm",
+        titulo: "Bolhas de proteção local podem ter freado Covid-19 em São Paulo",
+        data: "24/8/20",
+        fonte: "UOL"
+      },
+      {
+        link: "https://jornal.usp.br/ciencias/bolhas-de-protecao-local-podem-ter-freado-covid-19-em-sao-paulo-aponta-pesquisa/",
+        titulo: "Bolhas de proteção local podem ter freado Covid-19 em São Paulo, aponta pesquisa",
+        data: "24/8/20",
+        fonte: "Jornal da USP"
+      },
+      {
+        link: "https://dialogosdosul.operamundi.uol.com.br/saude/66351/bolhas-de-protecao-local-podem-ter-freado-covid-19-em-sao-paulo-diz-pesquisa-da-usp",
+        titulo: "Bolhas de proteção local podem ter freado Covid-19 em São Paulo, diz pesquisa da USP",
+        data: "25/8/20",
+        fonte: "Opera Mundi"
+      },
+      {
+        link: "https://www.ufabc.edu.br/ufabc-na-midia/bolhas-de-protecao-local-podem-ter-freado-covid-19-em-sao-paulo",
+        titulo: "Bolhas de proteção local podem ter freado Covid-19 em São Paulo",
+        data: "26/8/20",
+        fonte: "UFABC"
+      },
+      {
+        link: "https://www.dgabc.com.br/Noticia/3550436/estudo-da-ufabc-indica-que-bolhas-ajudam-frear-a-covid",
+        titulo: "Estudo da UFABC indica que bolhas ajudam a frear Covid",
+        data: "16/9/20",
+        fonte: "DIário do Grande ABC"
+      },
+      {
+        link: "https://www.em.com.br/app/noticia/internacional/bbc/2020/07/31/interna_internacional,1171782/brasil-3-motivos-explicam-por-que-casos-covid-19-voltaram-a-crescer.shtml",
+        titulo: "Três motivos explicam por que casos de Covid-19 voltaram a crescer",
+        data: "31/7/20",
+        fonte: "Estado de Minas"
+      },
+      {
+        link: "https://www.msnoticias.com.br/editorias/politica-mato-grosso-sul/entenda-o-porque-esta-sendo-egoista-ao-cobrar-celebracao-do-numero-de/100509/",
+        titulo: "Entenda o porquê está sendo egoísta ao cobrar celebração do número de curados",
+        data: "31/7/20",
+        fonte: "MS Notícias"
+      },
+      {
+        link: "https://www.revistaimagem.online/post/3-motivos-que-explicam-por-que-casos-de-covid-19-voltaram-a-crescer-no-brasil",
+        titulo: "Três motivos explicam por que casos de Covid-19 voltaram a crescer no Brasil",
+        data: "1/8/20",
+        fonte: "Revista Imagem"
+      },
+      {
+        link: "https://www.poder360.com.br/coronavirus/ate-46-de-alunos-e-funcionarios-se-infectariam-em-60-dias-de-aula-diz-estudo/",
+        titulo: "Até 46% de alunos e funcionários se infectariam em 60 dias de aula, diz estudo",
+        data: "22/8/20",
+        fonte: "Poder 360"
+      },
+      {
+        link: "https://www.institutounibanco.org.br/conteudo/pesquisa-estima-que-volta-as-aulas-pode-causar-infeccao-de-ate-46-de-alunos-por-covid/",
+        titulo: "Pesquisa estima que volta às aulas pode causar infecção de até 46% de alunos por Covid",
+        data: "25/8/20",
+        fonte: "Instituto Unibanco"
+      },
+      {
+        link: "https://portalhospitaisbrasil.com.br/bolhas-de-protecao-reduzem-infeccoes-na-capital-mas-podem-estourar/",
+        titulo: "Bolhas de proteção reduzem infecções na capital, mas podem estourar",
+        data: "16/7/20",
+        fonte: "Portal Hospitais Brasil"
+      },
+      {
+        link: "https://www.redebrasilatual.com.br/saude-e-ciencia/2020/08/bolhas-protecao-covid-sao-paulo/",
+        titulo: "‘Bolhas locais’ explicam redução da covid-19 em São Paulo, mas situação é instável",
+        data: "28/8/20",
+        fonte: "Rede Brasil Atual"
+      },
+      {
+        link: "https://oglobo.globo.com/sociedade/coronavirus/pesquisa-estima-que-volta-as-aulas-pode-causar-infeccao-de-ate-46-de-alunos-por-covid-24602179",
+        titulo: "Pesquisa estima que volta às aulas pode causar infecção de até 46% de alunos por Covid",
+        data: "23/8/20",
+        fonte: "O Globo"
+      },
+      {
+        link: "https://cnts.org.br/noticias/pesquisa-estima-que-volta-as-aulas-pode-causar-infeccao-de-ate-46-de-alunos-por-covid/",
+        titulo: "Pesquisa estima que volta às aulas pode causar infecção de até 46% de alunos por Covid",
+        data: "24/8/20",
+        fonte: "Confederação Nacional dos Trabalhadores na Saúde"
+      },
+      {
+        link: "https://exame.com/brasil/ate-metade-dos-alunos-poderia-se-infectar-com-aulas-em-sp-diz-estudo/",
+        titulo: "Até metade dos alunos poderia se infectar com aulas em SP, diz estudo",
+        data: "21/8/20",
+        fonte: "Exame"
+      },
+      {
+        link: "https://www.folhape.com.br/noticias/simulacao-calcula-que-ate-46-de-alunos-podem-ser-infectados-dois/151844/",
+        titulo: "Simulação calcula que até 46% de alunos podem ser infectados dois meses após volta às aulas",
+        data: "21/8/20",
+        fonte: "Folha de Pernambuco"
+      },
+      {
+        link: "https://www.al.sp.gov.br/noticia/?01/09/2020/aplicativo-simula-contaminacao-por-covid-19-conforme-caracteristicas-do-ambiente-escolar",
+        titulo: "Aplicativo simula contaminação por Covid-19 conforme características do ambiente escolar",
+        data: "1/9/20",
+        fonte: "Assembleia Legislativa do Estado de São Paulo"
+      },
+      {
+        link: "https://www.redebrasilatual.com.br/saude-e-ciencia/2020/08/volta-aulas-estudantes-professores-covid/",
+        titulo: "Volta às aulas: 46% dos estudantes e professores pegariam covid-19 em três meses",
+        data: "21/8/20",
+        fonte: "Rede Brasil Atual"
+      },
+      {
+        link: "https://cangurunews.com.br/simulador-transmissao-coronavirus-escolas/",
+        titulo: "Até 46% dos alunos podem ter infecção por covid após 2 meses de aula, calcula simulador",
+        data: "27/8/20",
+        fonte: "Canguru News"
+      },
+      {
+        link: "https://www.nexojornal.com.br/expresso/2020/09/02/Covid-por-que-os-n%C3%BAmeros-em-queda-no-Brasil-exigem-cautela",
+        titulo: "Por que os números em queda exigem cautela",
+        data: "2/9/20",
+        fonte: "Nexo"
+      },
+      {
+        link: "https://panoramafarmaceutico.com.br/2020/08/25/bolhas-de-protecao-local-podem-ter-freado-covid-19-em-sao-paulo-aponta-pesquisa/",
+        titulo: "Bolhas de proteção local podem ter freado Covid-19 em São Paulo, aponta pesquisa",
+        data: "25/8/20",
+        fonte: "Panorama Farmacêutico"
+      },
+      {
+        link: "https://coronavirus.atarde.com.br/ate-46-de-alunos-e-funcionarios-se-infectariam-em-60-dias-de-aula-diz-estudo/",
+        titulo: "Até 46% de alunos e funcionários se infectariam em 60 dias de aula, diz estudo",
+        data: "22/8/20",
+        fonte: "A Tarde"
+      },
+      {
+        link: "https://valor.globo.com/brasil/noticia/2020/08/21/simulacao-calcula-que-ate-46percent-dos-alunos-em-sp-podem-ter-covid-dois-meses-apos-volta-as-aulas.ghtml",
+        titulo: "Simulação calcula que até 46% dos alunos em SP podem ter Covid dois meses após volta às aulas",
+        data: "21/8/20",
+        fonte: "Valor Econômico"
+      },
+      {
+        link: "https://ootimista.com.br/covid-19/simulacao-calcula-que-ate-46-de-alunos-podem-ser-infectados-dois-meses-apos-volta-as-aulas/",
+        titulo: "Simulação calcula que até 46% dos alunos em SP podem ser infectados após volta às aulas",
+        data: "22/8/20",
+        fonte: "O Otimista"
+      },
+      {
+        link: "https://www.metrojornal.com.br/estilo-vida/2020/08/28/simulador-dispersao-virus.html",
+        titulo: "Volta às aulas: até 46% dos alunos podem se infectar pelo coronavírus após 2 meses",
+        data: "28/8/20",
+        fonte: "Metro Jornal"
+      },
+      {
+        link: "https://www.prensalatina.com.br/index.php?o=rn&id=34105&SEO=retorno-as-aulas-potencial-cenario-de-contagios-de-covid-no-brasil",
+        titulo: "Retorno às aulas, potencial cenário de contágios de Covid no Brasil",
+        data: "29/8/20",
+        fonte: "Prensa Latina"
+      },
+      {
+        link: "https://www.prensa-latina.cu/index.php?o=rn&id=396014&SEO=retorno-a-clases-potencial-escenario-de-contagios-de-covid-en-brasil",
+        titulo: "Retorno a clases, potencial escenario de contagios de Covid en Brasil",
+        data: "29/8/20",
+        fonte: "Prensa Latina ES"
+      },
+      {
+        link: "https://www.sindilex.org.br/nao-esta-tudo-bem/",
+        titulo: "Bolhas locais explicam redução da Covid em São Paulo, mas situação é instável",
+        data: "28/8/20",
+        fonte: "Sindilex"
+      },
+      {
+        link: "https://portalceleiro.com.br/noticia/12235/ate-46-dos-alunos-e-professores-podem-ser-infectados-apos-2-meses-de-aula-calcula-simulador.html",
+        titulo: "Até 46% dos alunos e professores podem ser infectados após 2 meses de aula, calcula simulador",
+        data: "28/8/20",
+        fonte: "Portal Celeiro"
+      },
+      {
+        link: "https://outraspalavras.net/outrasaude/meio-milhao-de-mortes-dez-milhoes-de-casos/",
+        titulo: "Meio milhão de mortes, dez milhões de casos",
+        data: "29/6/20",
+        fonte: "Outras Palavras"
+      },
+      {
+        link: "https://acidadeubatuba.com.br/noticia_static/5717/indefinicoes-sobre-volta-aulas-impulsionam-fenomen/",
+        titulo: "Indefinições sobre volta às aulas impulsionam fenômeno das microescolas",
+        data: "29/8/20",
+        fonte: "Jornal A Cidade"
+      },
+      {
+        link: "https://defatoonline.com.br/canais/educacao-e-carreira/noticias/2020/08/22/sp-46-dos-alunos-podem-pegar-coronavirus-dois-meses-apos-volta-as-aulas-diz-pesquisa/",
+        titulo: "Até 46% dos alunos podem pegar Covid dois meses após volta às aulas, diz pesquisa",
+        data: "22/8/20",
+        fonte: "De fato online"
+      },
+      {
+        link: "https://crn1.com.br/2020/08/quase-metade-dos-alunos-podem-ser-infectados-dois-meses-apos-volta-as-aulas-aponta-simulacao/",
+        titulo: "Quase metade dos alunos podem ser infectados dois meses após volta às aulas, aponta simulação",
+        data: "22/8/20",
+        fonte: "Central Regional de Notícias"
+      },
+      {
+        link: "https://www.causaoperaria.org.br/46-dos-alunos-serao-infectados-com-volta-as-aulas/",
+        titulo: "46% dos alunos serão infectados com volta às aulas",
+        data: "23/8/20",
+        fonte: "Causa Operária"
+      },
+      {
+        link: "http://www.informabahia.com.br/ate-46-de-alunos-e-funcionarios-se-infectariam-em-60-dias-de-aula-diz-estudo/",
+        titulo: "Até 46% de alunos e funcionários se infectariam em 60 dias de aula, diz estudo",
+        data: "22/8/20",
+        fonte: "Informa Bahia"
+      },
+      {
+        link: "http://diariooficial.imprensaoficial.com.br/doflash/prototipo/2020/Setembro/02/legislativo/pdf/pg_0004.pdf",
+        titulo: "Aplicativo simula contaminação por Covid-19 conforme características do ambiente escolar",
+        data: "2/9/20",
+        fonte: "Diário Oficial do Legislativo (SP)"
+      },
+      {
+        link: "https://www.cartacapital.com.br/educacao/escolas-podem-infectar-ate-46-dos-estudantes-dois-meses-apos-volta-as-aulas/",
+        titulo: "Escolas podem infectar até 46% dos alunos 2 meses após volta às aulas",
+        data: "21/8/20",
+        fonte: "Carta Capital"
+      },
+      {
+        link: "https://estudiohum.net/wp/cientista-dizem-que-46-de-alunos-e-funcionarios-se-infectariam-se-escolas-reabrirem/",
+        titulo: "Cientista dizem que 46% de alunos e funcionários se infectariam se escolas reabrirem",
+        data: "22/8/20",
+        fonte: "Estúdio Hum"
+      },
+      {
+        link: "https://www.transparencia.org.br/blog/transparencia-brasil-solicita-dados-mais-detalhados-sobre-covid-19-em-sp/#more-1485",
+        titulo: "Transparência Brasil solicita dados mais detalhados sobre Covid-19 em SP",
+        data: "16/7/20",
+        fonte: "Transparência Brasil"
+      },
+      {
+        link: "http://portuguese.people.com.cn/n3/2020/0825/c309808-9738205.html",
+        titulo: "Retorno das aulas aumentará transmissão de Covid-19, segundo pesquisas",
+        data: "25/8/20",
+        fonte: "Diário do Povo Online"
+      },
+      {
+        link: "https://www.todapalavra.info/post/simulacao-calcula-disseminacao-da-covid-19-nas-escolas",
+        titulo: "Simulação calcula disseminação de Covid nas escolas",
+        data: "21/8/20",
+        fonte: "Toda Palavra"
+      },
+      {
+        link: "https://www.dm.jor.br/cidades/2020/08/volta-as-aulas-covid-19-pode-infectar-quase-50-dos-alunos-segundo-pesquisa/",
+        titulo: "Volta às aulas: Covid-19 pode infectar quase 50% dos alunos, segundo pesquisa",
+        data: "24/8/20",
+        fonte: "DM"
+      },
+      {
+        link: "http://www.jornalatribuna.com.br/volta-as-aulas-infectara-ate-46-de-alunos-e-seus-parentes-em-casa-mesmo-com-protocolos/",
+        titulo: "Volta às aulas infectara até 46% de alunos e seus parentes em casa, mesmo com protocolos",
+        data: "24/8/20",
+        fonte: "A Tribuna"
+      },
+      {
+        link: "http://www.jornaldafranca.com.br/para-especialistas-volta-as-aulas-pode-infectar-quase-metade-dos-estudantes",
+        titulo: "Para especialistas, volta às aulas pode infectar quase metade dos estudantes",
+        data: "24/8/20",
+        fonte: "Jornal da Franca"
+      },
+      {
+        link: "https://www.gazetadopovo.com.br/republica/covid-19-brasil-descarta-bons-exemplos-estrategia-reabertura/",
+        titulo: "Brasil descarta “bons exemplos” e se alinha a México em estratégia de reabertura",
+        data: "10/6/20",
+        fonte: "Gazeta do Povo"
+      },
+      {
+        link: "https://www.tribunapr.com.br/noticias/brasil/simulacao-calcula-que-ate-46-de-alunos-podem-ser-infectados-dois-meses-apos-volta-as-aulas/",
+        titulo: "Quase metade dos alunos podem ser infectados dois meses após volta às aulas, aponta simulação",
+        data: "21/8/20",
+        fonte: "Tribuna do Paraná"
+      },
+      {
+        link: "https://www.dgabc.com.br/Noticia/3530621/estudo-aponta-que-volta-as-aulas-deve-ter-apenas-20-dos-estudantes",
+        titulo: "Estudo aponta que volta às aulas deve ter apenas 20% dos estudantes",
+        data: "26/8/20",
+        fonte: "Diário do Grande ABC"
+      },
+      {
+        link: "https://www.esquerdadiario.com.br/Para-72-aulas-presenciais-so-devem-ser-retomadas-quando-houver-vacina",
+        titulo: "Para 72%, aulas presenciais só devem ser retomadas quando houver vacina",
+        data: "7/9/20",
+        fonte: "Esquerda Diário"
+      },
+      {
+        link: "https://www.esquerdadiario.com.br/Com-apenas-35-da-capacidade-reabertura-das-escolas-pode-infectar-ate-46-dos-alunos",
+        titulo: "Com apenas 35% da capacidade, reabertura das escolas pode infectar até 46% dos alunos",
+        data: "24/8/20",
+        fonte: "Esquerda Diário"
+      },
+      {
+        link: "http://sind-proifes.org.br/posts/estudo-volta-aulas",
+        titulo: "Estudo aponta que volta às aulas deve ter apenas 20% dos estudantes",
+        data: "26/8/20",
+        fonte: "SindProifes"
+      },
+      {
+        link: "https://appsindicato.org.br/escolas-podem-infectar-ate-46-dos-alunos-2-meses-apos-volta-as-aulas/",
+        titulo: "Escolas podem infectar até 46% dos alunos 2 meses após volta às aulas",
+        data: "25/8/20",
+        fonte: "Sindicato dos Trabalhadores da Educação do Paraná"
+      },
+      {
+        link: "https://www.abrafi.org.br/index.php/site/noticiasnovo/ver/3704/educacao-superior",
+        titulo: "Simulação aponta que até 46% de alunos e professores podem ser infectados dois meses após volta às aulas na cidade de São Paulo",
+        data: "24/8/20",
+        fonte: "Abrafi"
+      },
+      {
+        link: "https://gestao.quero.com/blog/radar-da-educacao-cogna-planeja-reestruturacao-apos-prejuizo-de-r-452-milhoes/",
+        titulo: "Radar da Educação: Cogna planeja reestruturação após prejuízo de R$ 452 milhões",
+        data: "21/8/20",
+        fonte: "Quero Educação"
+      },
+      {
+        link: "https://www.estudarfora.org.br/volta-as-aulas-e-segura-na-pandemia-o-que-dizem-pesquisas-de-universidades-estrangeiras/",
+        titulo: "Volta às aulas é segura na pandemia? O que dizem pesquisas de universidades estrangeiras",
+        data: "27/8/20",
+        fonte: "Estudar Fora"
+      },
+      {
+        link: "https://jeduca.org.br/texto/relatorio-da-ocde-analisa-impacto-da-pandemia-na-educacao",
+        titulo: "Relatório da OCDE analisa impacto da pandemia na educação",
+        data: "8/9/20",
+        fonte: "Jeduca"
+      },
+      {
+        link: "https://noticiasconcursos.com.br/noticias-concursos/alunos-coronavirus-volta-as-aulas/",
+        titulo: "SP: 46% dos alunos podem pegar coronavírus dois meses após volta às aulas, diz pesquisa",
+        data: "22/8/20",
+        fonte: "Notícias Concursos"
+      },
+      {
+        link: "https://www.facebook.com/carlosgiannazioficial/videos/365778111083452/",
+        titulo: "Não ao genocídio na educação",
+        data: "27/8/20",
+        fonte: "Carlos Giannazi"
+      },
+    {
         link: "https://radioagencianacional.ebc.com.br/saude/audio/2020-06/sao-paulo-registra-novo-recorde-de-mortes-e-de-novos-casos-de-coronavirus",
         titulo: "Após flexibilização da quarentena, São Paulo registra novo recorde de mortes e casos de coronavírus",
         data: "02/06/2020",
